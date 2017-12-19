7cca4f61e6d18c51b22fa74a0adb0f8f23945f50
pooi <ldayou@naver.com>
2017-12-19 17:06:12 +0900

    Initial commit

--- app/src/main/java/ga/discoveryandlost/discoveryandlost/MainActivity.java
+++ app/src/main/java/ga/discoveryandlost/discoveryandlost/MainActivity.java
@@ -1,13 +1,163 @@
 package ga.discoveryandlost.discoveryandlost;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.gigamole.navigationtabbar.ntb.NavigationTabBar;
+
+import java.util.ArrayList;
+
+import ga.discoveryandlost.discoveryandlost.obj.User;
+
+public class MainActivity extends BaseActivity {
+
+    private ViewPager viewPager;
+    private NavigationAdapter mPagerAdapter;
+    private String[] toolbarTitleList = new String[3];
+    private static final int startPage = 0;
+
+    private User user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbarTitleList = new String[3];
+        toolbarTitleList[0] = getString(R.string.manage_srt);
+        toolbarTitleList[1] = getResources().getString(R.string.view_by_item);
+        toolbarTitleList[2] = getResources().getString(R.string.view_by_date);
+
+        user = StartActivity.USER;
+
+        init();
+
     }
+
+
+    private void init(){
+
+        viewPager = (ViewPager) findViewById(R.id.vp_horizontal_ntb);
+        viewPager.setOffscreenPageLimit(toolbarTitleList.length);
+        mPagerAdapter = new NavigationAdapter(getSupportFragmentManager(), user);
+        viewPager.setAdapter(mPagerAdapter);
+
+        final String[] colors = getResources().getStringArray(R.array.default_preview);
+        final NavigationTabBar navigationTabBar = (NavigationTabBar) findViewById(R.id.ntb_horizontal);
+        navigationTabBar.setTitleSize(30);
+        final ArrayList<NavigationTabBar.Model> models = new ArrayList<>();
+
+        models.add(
+                new NavigationTabBar.Model.Builder(
+                        getResources().getDrawable(R.drawable.ic_file_document_white_36dp),
+                        getColorId(R.color.colorAccent))
+                        .build()
+        );
+        models.add(
+                new NavigationTabBar.Model.Builder(
+                        getResources().getDrawable(R.drawable.ic_format_list_bulleted_type_white_36dp),
+                        getColorId(R.color.colorAccent))
+                        .build()
+        );
+        models.add(
+                new NavigationTabBar.Model.Builder(
+                        getResources().getDrawable(R.drawable.ic_calendar_today_white_36dp),
+                        getColorId(R.color.colorAccent))
+                        .build()
+        );
+
+        navigationTabBar.setModels(models);
+        navigationTabBar.setViewPager(viewPager, startPage);
+        navigationTabBar.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(final int position, final float positionOffset, final int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(final int position) {
+                navigationTabBar.getModels().get(position).hideBadge();
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(final int state) {
+
+            }
+        });
+
+        navigationTabBar.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < navigationTabBar.getModels().size(); i++) {
+                    final NavigationTabBar.Model model = navigationTabBar.getModels().get(i);
+                    navigationTabBar.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            model.showBadge();
+                        }
+                    }, i * 100);
+                }
+            }
+        }, 500);
+
+    }
+
+
+
+
+    private static class NavigationAdapter extends FragmentStatePagerAdapter {
+
+        User user;
+
+        public NavigationAdapter(FragmentManager fm, User user){
+            super(fm);
+            this.user = user;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            Fragment f;
+            final int pattern;
+            pattern = position%3;
+            Bundle bdl = new Bundle(1);
+            bdl.putSerializable("user", user);
+
+            switch (pattern) {
+                case 0: {
+                    f = new Fragment();
+                    f.setArguments(bdl);
+                    break;
+                }
+                case 1: {
+                    f = new Fragment();
+                    f.setArguments(bdl);
+                    break;
+                }
+                case 2: {
+                    f = new Fragment();
+                    f.setArguments(bdl);
+                    break;
+                }
+                default: {
+                    f = new Fragment();
+                    break;
+                }
+            }
+
+            return f;
+        }
+
+        @Override
+        public int getCount(){
+            return 3;
+        }
+
+    }
+
 }
